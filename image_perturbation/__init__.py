6eab1c38d2e04e97ede3fa2a25b161a54d501e05
carlosejimenez <cjsaltlake@gmail.com>
2021-01-19 00:15:19 -0800

    add setuptools

---
+++ image_perturbation/__init__.py
@@ -0,0 +1,7 @@
+from . import image_perturbation
+from . import utils
+from . import modeling_frcnn
+from . import preprocess_image
+from .image_perturbation import ImageBuffer
+
+__all__ = [image_perturbation, utils, preprocess_image, modeling_frcnn]
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='image_perturbation',
+      version='0.1',
+      description='Image Buffer with Faster R-CNN module.',
+      url='http://github.com/carlosejimenez/imageperturbation',
+      author='Carlos E. Jiménez',
+      author_email='cjsaltlake@gmail.com',
+      license='MIT',
+      packages=['image_perturbation'],
+      zip_safe=False)
