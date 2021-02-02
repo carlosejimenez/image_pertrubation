2c2860fee7a8e2c388fc02bcf693b3a70d40ef78
carlosejimenez <cjsaltlake@gmail.com>
2021-02-02 03:56:37 -0800

    update

--- image_perturbation/image_perturbation.py
+++ image_perturbation/image_perturbation.py
@@ -120,7 +120,6 @@ def blur_context(img, sigma, bboxes):
 
 
 def avg_context(img, bboxes):
-    print('Hello, in avg!')
     img_avgd = np.ones_like(img) * AVERAGES
     objs_mask = np.zeros_like(img, dtype=bool)
     for bbox in bboxes:
