ce5b3b4267a53af8b1040d23e2a7ea7446b489c7
carlosejimenez <cjsaltlake@gmail.com>
2021-02-08 21:25:11 -0800

    Add ImageProcessor to __init__.py file.

--- image_perturbation/__init__.py
+++ image_perturbation/__init__.py
@@ -3,5 +3,6 @@ from . import utils
 from . import modeling_frcnn
 from . import preprocess_image
 from .image_perturbation import ImageBuffer
+from .image_perturbation import ImageProcessor
 
 __all__ = [image_perturbation, utils, preprocess_image, modeling_frcnn]
